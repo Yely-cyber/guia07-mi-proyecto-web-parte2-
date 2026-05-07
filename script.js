eb637236b4b6c7359c8518ba93db247f35491f40
Yely-cyber <230252@unsaac.edu.pe>
2026-05-06 23:08:49 -0500

    Version inicial del primer ejercicio propuesto

---
+++ script.js
@@ -0,0 +1,24 @@
+function saludar() {
+  // Obtener el valor del input
+  let nombre = document.getElementById("nombre").value;
+
+  // Validar si el usuario escribió algo
+  if (nombre === "") {
+    document.getElementById("resultado").innerText =
+      "Por favor, ingresa tu nombre.";
+  } else {
+    document.getElementById("resultado").innerText =
+      "Hola " + nombre + ", bienvenido al sistema.";
+  }
+}
+function validarCorreo() {
+  let correo = document.getElementById("correo").value;
+
+  if (correo === "") {
+    document.getElementById("mensajeCorreo").innerText =
+      "Debe ingresar un correo.";
+  } else {
+    document.getElementById("mensajeCorreo").innerText =
+      "Correo registrado correctamente.";
+  }
+}

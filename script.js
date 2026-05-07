425c35dd406c2c2e60e42a017ee4a9064e1932e3
Yely-cyber <230252@unsaac.edu.pe>
2026-05-07 00:08:56 -0500

    Se valida longitud minima y caracteres de nombre y appellido

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function saludar() {
-  let nombre = document.getElementById("nombre").value;
-  let apellido = document.getElementById("apellido").value;
+  let nombre = document.getElementById("nombre").value.trim();
+  let apellido = document.getElementById("apellido").value.trim();
+  let soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+
   if (nombre === "") {
     document.getElementById("resultado").innerText = "Por favor, ingresa tu nombre.";
-  } else if (apellido === "") {
-    document.getElementById("resultado").innerText = "Por favor, ingresa tu apellido.";
-  }else {
-    document.getElementById("resultado").innerText = "Hola " + nombre + apellido + ", bienvenido al sistema.";
+  } else if (nombre.length < 3) {
+    document.getElementById("resultado").innerText = "El nombre debe tener al menos 3 caracteres.";
+  } else if (!soloLetras.test(nombre)) {
+    document.getElementById("resultado").innerText = "El nombre solo debe contener letras.";
+  } else {
+    let nombreFormateado = nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+    document.getElementById("resultado").innerText = "Hola " + nombreFormateado + ", bienvenido al sistema.";
   }
+
+  if (apellido === "") {
+    document.getElementById("resultado").innerText = "Por favor, ingresa tu apellido.";
+  } else if (apellido.length < 4) {
+    document.getElementById("resultado").innerText = "El apellido debe tener al menos 4 caracteres.";
+  } else if (!soloLetras.test(apellido)) {
+    document.getElementById("resultado").innerText = "El apellido solo debe contener letras.";
+  } else {
+    let apellidoFormateado = apellido.charAt(0).toUpperCase() + apellido.slice(1).toLowerCase();
+    document.getElementById("resultado").innerText = "Hola " + apellidoFormateado + ", bienvenido al sistema.";
+  }
+  
 }
 
 function validarCorreo() {
